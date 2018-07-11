d393d16c4bc5134859d3c47b4d6fca6d0b304334
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-07-11 08:07:19 +0300

    added test request

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    const WebAssembly = window.WebAssembly;
+    WebAssembly.instantiateStreaming(fetch("./test.wasm"), {
+      env: {
+        sayHello() {
+          console.log("Hello from WebAssembly!");
+        },
+        abort(msg, file, line, column) {
+          console.error("abort called at main.ts:" + line + ":" + column);
+        }
+      },
+      console: {
+        logi(value) { console.log('logi: ' + value); },
+        logf(value) { console.log('logf: ' + value); }
+      }
+    }).then(result => {
+      const exports = result.instance.exports;
+      document.getElementById("container").innerText = "Result: " + exports.add(19, 23);
+    }).catch(console.error);
+  }
+
   render() {
     return (
       <div className="App">
