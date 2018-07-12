b00d317316d07b21df250656da5cea48a7ab6b72
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-07-12 08:09:13 +0300

    working with json

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const _ = require('lodash');
+
+const users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
+  if (err) throw err
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello worldik');
+  let buffer = '';
+  users.map(u => {
+    buffer += `<a href="/${u.username}">${u.name.first} ${u.name.last}</a><br>`
+  });
+  res.send(buffer);
+});
+
+
+app.get('/:username', (req, res) => {
+  const username = req.params.username;
+  res.send(username);
 });
 
 var server = app.listen(3000, () => {
