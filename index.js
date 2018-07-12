87f57dac83e34068cc7c107591ea9bcfb7610f18
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-07-12 08:47:28 +0300

    run wasm

--- index.js
+++ index.js
@@ -2,33 +2,10 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const _ = require('lodash');
-
-const users = [];
-
-fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
-  if (err) throw err
-  JSON.parse(data).forEach(function (user) {
-    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-    users.push(user)
-  })
-})
+const path = require('path');
 
 
-app.set('views', './views');
-app.set('view engine', 'jade');
-
-app.get('/', (req, res) => {
-  console.log(1);
-  res.render('index', {users: users});
-});
-
-app.get('/:username', (req, res) => {
-  const username = req.params.username;
-  res.send(username);
-});
-
-
-
-var server = app.listen(3000, () => {
-  console.log(`Server running at http://localhost:${server.address().port}`);
+app.use(express.static(path.join(__dirname + '')));
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
