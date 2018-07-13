e470eae54ac4b21a0598ed3123e36880c5a1ef61
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-07-13 08:07:25 +0300

    working with memory

--- loadWasm.js
+++ loadWasm.js
@@ -18,3 +18,25 @@ fetchAndInstantiateWasm('./program.wasm', {
 .then(m => {
   console.log(m.getSqrt(25));
 });
+
+fetchAndInstantiateWasm('./toLowerCase.wasm', {
+  env: {
+    consoleLog (offset, len) {
+      const strBuf = new Uint8Array(mem.buffer, offset, len);
+      log(new TextDecoder().decode(strBuf));
+    }
+  }
+})
+.then(m => {
+  const mem = m.exports.memory;
+  function writeString(str, offset) {
+    const strBuf = new TextEncoder().encode(str);
+    const outBuf = new Uint8Array(mem.buffer, offset, strBuf.length);
+    for (let i = 0; i < strBuf.length; i++) {
+      outBuf[i] = strBuf[i];
+    }
+  }
+  const str = "Hello World";
+  writeString(str, wasmInstance.exports.getInStrOffset());
+  wasmInstance.exports.toLowerCase();
+});
