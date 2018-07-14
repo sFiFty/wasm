43588354b5d65fc83dc96ac2a26ec7d059358e33
sFiFty <alexandr.rudin.my@gmail.com>
2018-07-14 19:55:07 +0300

    added one more wasm module

---
+++ fib.c
@@ -0,0 +1,13 @@
+int fib(int n) 
+{
+  if (n == 0) { return 0; }
+  else 
+  {
+    if ((n == -1) || (n == 1)) { return 1; }
+    else 
+    {
+      if (n > 0) { return fib(n - 1) + fib(n - 2); }
+      else { return fib(n + 2) - fib(n + 1); }
+    }
+  }
+}
