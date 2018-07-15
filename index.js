e83da31134c4e4979393d1f13f38d0ddefa7062e
sFiFty <alexandr.rudin.my@gmail.com>
2018-07-15 13:30:35 +0300

    test emscripten

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const path = require('path');
 
 
 app.use(express.static(path.join(__dirname + '')));
+app.get('/fib', (req, res) => {
+  res.sendFile(path.join(__dirname+'/fib.html'))
+});
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
