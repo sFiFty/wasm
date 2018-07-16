26a5fb69eb24d5f19898efec6852dea089085025
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-07-16 07:52:57 +0300

    added express endpoint

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.use(express.static(path.join(__dirname + '')));
 app.get('/fib', (req, res) => {
   res.sendFile(path.join(__dirname+'/fib.html'))
 });
+
+app.get('/add', (req, res) => {
+  const buf = fs.readFileSync('./add.wasm');
+  const wasmModule = new WebAssembly.Module(new Uint8Array(buf));
+  const wasmInstance = new WebAssembly.Instance(wasmModule);
+  console.log(wasmInstance.exports.add(2,3));
+});
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
